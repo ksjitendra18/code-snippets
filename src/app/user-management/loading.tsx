0bd126de7e75b91c632212e39bba79711027be2d
Jitendra <jitendraks1803@gmail.com>
2026-06-23 21:12:27 +0530

    fixed

--- src/app/user-management/loading.tsx
+++ src/app/user-management/loading.tsx
@@ -1,9 +1,19 @@
-import { Loader2 } from "lucide-react";
-
 export default function UserManagementLoading() {
   return (
-    <>
-      <Loader2 className="animate-spin " />
-    </>
+    <div className="flex flex-col md:max-w-1/2 px-4 mt-10 mx-auto">
+      <div className="h-9 w-48 bg-gray-200 rounded-md animate-pulse mx-auto my-5" />
+
+      <div className="mt-4">
+        <div className="h-5 w-16 bg-gray-200 rounded animate-pulse mb-2" />
+        <div className="h-10 w-full bg-gray-200 rounded-md animate-pulse" />
+      </div>
+
+      <div className="mt-4">
+        <div className="h-5 w-12 bg-gray-200 rounded animate-pulse mb-2" />
+        <div className="h-10 w-full bg-gray-200 rounded-md animate-pulse" />
+      </div>
+
+      <div className="h-10 w-full bg-gray-200 rounded-md animate-pulse my-5" />
+    </div>
   );
 }
